34f5141a67ab8c6443a2ad3fbd2a6d0377b55c59
NikitaTarasenko <36858449+NikitaTarasenko@users.noreply.github.com>
2021-12-04 17:47:40 +0200

    Updates

---
+++ precache-manifest.9896743fe2e5e1e6b9dc58632b90f30e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "3976cdf80f3271d3bb32",
+    "url": "/react-GoT/static/css/main.ec472ef1.chunk.css"
+  },
+  {
+    "revision": "3976cdf80f3271d3bb32",
+    "url": "/react-GoT/static/js/main.3976cdf8.chunk.js"
+  },
+  {
+    "revision": "a1380c1dfc0c7148d620",
+    "url": "/react-GoT/static/css/1.85e1bde0.chunk.css"
+  },
+  {
+    "revision": "a1380c1dfc0c7148d620",
+    "url": "/react-GoT/static/js/1.a1380c1d.chunk.js"
+  },
+  {
+    "revision": "636753ef6fc0f3538cf6",
+    "url": "/react-GoT/static/js/runtime~main.636753ef.js"
+  },
+  {
+    "revision": "cdbfd1144b04e2c56fa0b27f1a17547b",
+    "url": "/react-GoT/static/media/jon.cdbfd114.jpg"
+  },
+  {
+    "revision": "bab285dedd414e158dad7ba7c78fbb1a",
+    "url": "/react-GoT/static/media/bg.bab285de.jpg"
+  },
+  {
+    "revision": "4cdbceae1a84901c02ba27a652c1fedf",
+    "url": "/react-GoT/index.html"
+  }
+];
